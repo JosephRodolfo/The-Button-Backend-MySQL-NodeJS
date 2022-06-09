ad7e4b93453bbc1c3ea7e7b3bd3375d9656f1d66
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-06-09 13:14:54 -0400

    switch droppnig and copying table to deleting and just copying entries because it was causing unintended problems with multiple clients connected

--- src/models/HighScores.model.js
+++ src/models/HighScores.model.js
@@ -4,7 +4,7 @@ const HighScores = function (table) {
 };
 HighScores.copyTable = (result) => {
   sql.query(
-   `CREATE TABLE IF NOT EXISTS highScores AS SELECT * FROM user;`,
+   `INSERT INTO highScores SELECT * FROM user`,
     (err, res) => {
         if (err) {
             console.log("error: ", err);
@@ -18,7 +18,7 @@ HighScores.copyTable = (result) => {
 
 HighScores.dropTable = (result) => {
     sql.query(
-      `DROP TABLE IF EXISTS highScores`,
+      `TRUNCATE highScores`,
       (err, res) => {
           if (err) {
               console.log("error: ", err);
